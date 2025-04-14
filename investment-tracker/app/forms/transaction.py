7e05f09de7207394557aaa77ae1f99fe948555ce
Francesco <francesco.sacripanti@gmail.com>
2025-04-14 21:53:15 +0000

    pausa

--- investment-tracker/app/forms/transaction.py
+++ investment-tracker/app/forms/transaction.py
@@ -1,11 +1,26 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import SubmitField
+from wtforms import DecimalField, SelectField, SubmitField, TextAreaField, FileField
+from wtforms.fields import DateField
+from wtforms.validators import DataRequired, NumberRange
+from flask_wtf.file import FileAllowed
+
+
+class TransactionForm(FlaskForm):
+    transaction_date = DateField('Data operazione', format='%Y-%m-%d', validators=[DataRequired()])
+    transaction_type = SelectField('Tipo di transazione', choices=[
+        ('BUY', 'Acquisto'),
+        ('SELL', 'Vendita')
+    ], validators=[DataRequired()])
+    shares = DecimalField('Quantità (azioni)', validators=[DataRequired(), NumberRange(min=0)])
+    price = DecimalField('Prezzo per azione', validators=[DataRequired(), NumberRange(min=0)])
+    commission = DecimalField('Commissioni', validators=[NumberRange(min=0)], default=0)
+    notes = TextAreaField('Note')
+    submit = SubmitField('Salva')
+
 
 class ImportTransactionsForm(FlaskForm):
-    """Form per l'importazione di transazioni da file CSV"""
     csv_file = FileField('File CSV', validators=[
-        FileRequired(),
-        FileAllowed(['csv'], 'Solo file CSV sono supportati')
+        DataRequired(),
+        FileAllowed(['csv'], 'Solo file CSV!')
     ])
     submit = SubmitField('Importa')
